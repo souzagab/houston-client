997df4da55dcd39c8952917b8784fdcd427b0276
Gabriel S. <thesouzagab@gmail.com>
2023-11-24 15:16:11 -0300

    Create new gem

---
+++ spec/houston/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Houston::Client do
+  it "has a version number" do
+    expect(Houston::Client::VERSION).not_to be_nil
+  end
+
+  it "does something useful" do
+    expect(false).to be(true)
+  end
+end
