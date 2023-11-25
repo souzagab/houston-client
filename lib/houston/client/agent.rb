b8906bed6cffd0c8b8576c90256e41eacfe44636
Gabriel S. <thesouzagab@gmail.com>
2023-11-25 11:22:34 -0300

    chore: add http method to agent

--- lib/houston/client/agent.rb
+++ lib/houston/client/agent.rb
@@ -7,10 +7,40 @@ module Houston
 
       setting :url
 
-      def self.base_url
-        raise ConfigurationError, "Houston::Client::Agent.url is not configured" unless config.url
+      class << self
+        def get(path, params = {})
+          url = "#{base_url}/#{path}"
 
-        config.url
+          response = http.get(url, params)
+
+          parse_response(response)
+        end
+
+        def post(path, params = {})
+          url = "#{base_url}/#{path}"
+
+          response = http.post(url, json: params)
+
+          parse_response(response)
+        end
+
+        private
+
+        def base_url
+          raise ConfigurationError, "Houston::Client::Agent.url is not configured" unless config.url
+
+          config.url
+        end
+
+        def http
+          HTTP
+            .headers("User-Agent" => "Houston::Client/#{Houston::Client::VERSION}")
+            .accept(:json)
+        end
+
+        def parse_response(response)
+          JSON.parse(response.body.to_s, symbolize_names: true)
+        end
       end
     end
   end
