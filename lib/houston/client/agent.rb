420989356bb79c398e7decc2032c5a7f57d5c0ea
Gabriel S. <thesouzagab@gmail.com>
2023-11-28 15:33:55 -0300

    doc: add yard docs

--- lib/houston/client/agent.rb
+++ lib/houston/client/agent.rb
@@ -4,45 +4,63 @@ require "json"
 
 module Houston
   module Client
+    # The Agent class is responsible for sending HTTP requests to a specified URL.
     class Agent
       extend Dry::Configurable
 
       setting :url
 
-      class << self
-        def get(path, params = {})
-          url = "#{base_url}/#{path}"
+      # Sends a GET request to the specified path with optional parameters.
+      #
+      # @param path [String] The path to send the GET request to.
+      # @param params [Hash] Optional parameters to include in the request.
+      # @return [Hash] The parsed JSON response.
+      def self.get(path, params = {})
+        url = "#{base_url}/#{path}"
 
-          response = http.get(url, params)
+        response = http.get(url, params)
 
-          parse_response(response)
-        end
+        parse_response(response)
+      end
 
-        def post(path, params = {})
-          url = "#{base_url}/#{path}"
+      # Sends a POST request to the specified path with optional parameters.
+      #
+      # @param path [String] The path to send the POST request to.
+      # @param params [Hash] Optional parameters to include in the request.
+      # @return [Hash] The parsed JSON response.
+      def self.post(path, params = {})
+        url = "#{base_url}/#{path}"
 
-          response = http.post(url, json: params)
+        response = http.post(url, json: params)
 
-          parse_response(response)
-        end
+        parse_response(response)
+      end
 
-        private
+      # Retrieves the base URL from the configuration and raises an error if it is not configured.
+      #
+      # @raise [ConfigurationError] If Houston::Client::Agent.url is not configured.
+      # @return [URI] The parsed base URL.
+      def self.base_url
+        raise ConfigurationError, "Houston::Client::Agent.url is not configured" unless config.url
 
-        def base_url
-          raise ConfigurationError, "Houston::Client::Agent.url is not configured" unless config.url
+        URI.parse(config.url)
+      end
 
-          URI.parse(config.url)
-        end
+      # Returns an instance of the HTTP client with the necessary headers and content type.
+      #
+      # @return [HTTP::Client] The HTTP client instance.
+      def self.http
+        HTTP
+          .headers("User-Agent" => "Houston::Client/#{Houston::Client::VERSION}")
+          .accept(:json)
+      end
 
-        def http
-          HTTP
-            .headers("User-Agent" => "Houston::Client/#{Houston::Client::VERSION}")
-            .accept(:json)
-        end
-
-        def parse_response(response)
-          JSON.parse(response.body.to_s, symbolize_names: true)
-        end
+      # Parses the response body as JSON and returns the parsed result.
+      #
+      # @param response [HTTP::Response] The HTTP response object.
+      # @return [Hash] The parsed JSON response.
+      def self.parse_response(response)
+        JSON.parse(response.body.to_s, symbolize_names: true)
       end
     end
   end
--- lib/houston/client/spacecraft.rb
+++ lib/houston/client/spacecraft.rb
@@ -1,8 +1,14 @@
 module Houston
   module Client
+    # Represents a spacecraft in the Houston client library.
     class Spacecraft
+      # The path to the spacecrafts endpoint.
       BASE_PATH = "spacecrafts".freeze
 
+      # Retrieves the missions associated with a spacecraft.
+      #
+      # @param spacecraft_id [String] The ID of the spacecraft.
+      # @return [Hash] The missions associated with the spacecraft.
       def self.missions(spacecraft_id)
         missions_path = "missions"
 
